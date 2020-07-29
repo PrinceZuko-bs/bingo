820b9c63c21f3bc61a6e14148dd6b8f10a95980d
PrinceZuko-bs <brendan.schierloh@hey.com>
2020-07-29 15:53:03 -0700

    Added JS and CSS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,10 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+
+//console.log('Hello from application.js')
+
 
 
 
---
+++ app/javascript/packs/my_js.js
@@ -0,0 +1,59 @@
+console.log('Hello from advanced JS')
+
+$('li').on('click', function() {
+var toggle = $(this).attr('class');
+  if(toggle == 'hot'){
+      $(this).css('background-color', '#26deaa');
+       $(this).attr('class', 'cool');
+  }
+  else if(toggle == 'cool'){
+    $(this).css('background-color', '#ed5182');
+    $(this).attr('class', 'hot');
+  }
+
+});
+
+
+$('h1').on('click', function() {
+var toggle = $(this).attr('class');
+  if(toggle == 'hot'){
+      $(this).css('background-color', '#26deaa');
+       $(this).attr('class', 'cool');
+  }
+  else if(toggle == 'cool'){
+    $(this).css('background-color', '#ed5182');
+    $(this).attr('class', 'hot');
+  }
+
+
+});
+
+
+function sleep(ms) {
+  return new Promise(
+    resolve => setTimeout(resolve, ms)
+  );
+}
+
+
+$('div#left').on('click', async function() {
+  $(this).css('background-color', 'blue');
+  await sleep(700);
+  removeOptions();
+
+});
+
+
+$('div#right').on('click', async function() {
+  $(this).css('background-color', 'blue');
+  await sleep(700);
+  removeOptions();
+});
+
+
+function removeOptions(){
+  $('li#checkAnswer').css('background-color', '#403c3b');
+  $('li#checkAnswer').text('');
+  $('div#left').fadeOut();
+  $('div#right').fadeOut();
+}
